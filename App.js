2b849842752b282fe41833d264c08790531206c6
UrminGabani <91885281+UrminGabani@users.noreply.github.com>
2023-11-24 14:54:26 +0530

    changes

--- App.js
+++ App.js
@@ -21,28 +21,64 @@ const App = () => {
     const cameraPermission = Camera.requestCameraPermission();
     Camera.getCameraPermissionStatus().then(v => setCameraPermission(v));
   }, []);
+  // GoogleSignin.configure({
+  //   webClientId:
+  //     '568591053508-1eh48q3d3nju4nkgd759lajpmnri0sut.apps.googleusercontent.com',
+  //   offlineAccess: false,
+  //   profileImageSize: PROFILE_IMAGE_SIZE,
+  // });
+
   GoogleSignin.configure({
+    scopes: [
+      'https://www.googleapis.com/auth/youtube',
+      'https://www.googleapis.com/auth/youtube.readonly',
+    ], // what API you want to access on behalf of the user, default is email and profile
     webClientId:
-      '568591053508-1eh48q3d3nju4nkgd759lajpmnri0sut.apps.googleusercontent.com',
-    offlineAccess: false,
-    profileImageSize: PROFILE_IMAGE_SIZE,
+      '407570391570-8tt7tn1f6s52g92v2v108u893elfpo06.apps.googleusercontent.com',
+    offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+    hostedDomain: '', // specifies a hosted domain restriction
+    forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+    accountName: '', // [Android] specifies an account name on the device that should be used
+    // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+    googleServicePlistPath: '', // [iOS] if you renamed your GoogleService-Info file, new name here, e.g. GoogleService-Info-Staging
+    openIdRealm: '', // [iOS] The OpenID2 realm of the home web server. This allows Google to include the user's OpenID Identifier in the OpenID Connect ID token.
+    profileImageSize: 120, // [iOS] The desired height (and width) of the profile image. Defaults to 120px
   });
 
-  const createLiveBroadcast = async () => {
+  const createLiveBroadcast = async accessToken => {
     const response = await fetch(
-      `https://www.googleapis.com/youtube/v3/liveBroadcasts?part=id,snippet&key=AIzaSyCNgo6F2hlHxEt2o9XXY2jeI_vYDqZcpnc`,
+      `https://www.googleapis.com/youtube/v3/liveBroadcasts?part=id,snippet&key=AIzaSyCP2WemHVqYALBw-t47RzFUSYwZAPrwlGQ`,
+      {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
     );
     const data = await response.json();
+
     console.log(JSON.stringify(data, null, 2));
     return data;
+  };
+
+  const signOut = async () => {
+    try {
+      await GoogleSignin.signOut();
+      // setState({ user: null }); // Remember to remove the user from your app's state as well
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const signIn = async () => {
     try {
       await GoogleSignin.hasPlayServices();
       const userInfo = await GoogleSignin.signIn();
-      console.log(userInfo);
+      const {accessToken} = await GoogleSignin.getTokens();
       // setState({ userInfo });
+      createLiveBroadcast(accessToken);
     } catch (error) {
       if (error.code === statusCodes.SIGN_IN_CANCELLED) {
         // user cancelled the login flow
@@ -69,7 +105,7 @@ const App = () => {
           frameProcessorFps={5}
         />
       )}
-      <Button title="Hello" onPress={() => createLiveBroadcast()} />
+      <Button title="Hello" onPress={() => signIn()} />
     </View>
   );
 };
